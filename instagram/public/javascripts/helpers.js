04b5e8a63d612c588225f1d9e75fda64abb27081
Jose Franny Rodriguez <icancode911@gmail.com>
2017-06-08 15:41:40 -0400

    added the comment box,functionality and replaces some icons

--- instagram/public/javascripts/helpers.js
+++ instagram/public/javascripts/helpers.js
@@ -1,9 +1,4 @@
-function clearfunc(){
-
-
-  console.log(hey);
-}
-
+$('#targ').hide();
 $('#search-input').focus(function(){
   var hey = $(".fa-search");
   hey.hide();
@@ -11,4 +6,11 @@ $('#search-input').focus(function(){
 $('#search-input').blur(function(){
   var hey = $(".fa-search");
   hey.show();
+})
+
+function commentboxshow(){
+  $('#targ').show();
+}
+$("#textarea").focus(function(){
+  var text = $("#textarea").val("");
 })
