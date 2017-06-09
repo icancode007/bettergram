a7c863801ac13a95acda8e817542dac9652542ed
Jose Franny Rodriguez <icancode911@gmail.com>
2017-06-08 20:08:03 -0400

    added sequelize modularity and migrations for users model

---
+++ instagram/models/user.js
@@ -0,0 +1,16 @@
+'use strict';
+module.exports = function(sequelize, DataTypes) {
+  var user = sequelize.define('user', {
+    first_name: DataTypes.STRING,
+    last_name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    phone: DataTypes.STRING
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    }
+  });
+  return user;
+};
