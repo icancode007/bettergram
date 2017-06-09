3b9e4b583ca7fc8a1fe8b698d6fb079931c5d011
Jose Franny Rodriguez <icancode911@gmail.com>
2017-06-08 20:43:58 -0400

    tested migrations and table in psql

--- instagram/app.js
+++ instagram/app.js
@@ -4,7 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var models = require('./models/index');
 
 //install the routes
 var index = require('./routes/index');
--- instagram/routes/index.js
+++ instagram/routes/index.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var models = require('../models/index');
+var User = models.user;
 var router = express.Router();
 
 /* GET home page. */
@@ -6,4 +8,13 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Instagram' });
 });
 
+router.post('/',function(req,res,next){
+  User.create({
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+      email: req.body.email
+  }).then(function(user){
+    res.redirect('/users')
+  });
+})
 module.exports = router;
