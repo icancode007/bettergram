0651e44fc3182b556e42b84a536ff92f15408ed4
Jose Franny Rodriguez <icancode911@gmail.com>
2017-06-15 07:24:11 -0400

    starting over with something new for hte img upload

--- instagram/routes/users.js
+++ instagram/routes/users.js
@@ -1,16 +1,10 @@
 var express = require('express');
+var models = require('../models/index');
 var multer = require('multer');
 var sharp = require('sharp');
 var models = require('../models/index');
 var User = models.user;
 var uploadHandler = multer();
-
-cloudinary.config({ 
-  cloud_name: 'diqhgzinu', 
-  api_key: '861668197961751', 
-  api_secret: 'LtbjNc1kVDWszRQ9UYYWrBERL0s' 
-});
-
 
 var router = express.Router();
 
@@ -30,29 +24,3 @@ router.get('/:username', function(req, res) {
     });
 });
 
-router.post('/:username', uploadHandler.single('image'), function(req, res) {
-            User.findOne({
-                where: {
-                    username: req.params.username
-                }
-            }).then(function(user) {
-              console.log("user :" + user);
-                user.update({
-                    bio: req.body.bio
-                }).then(function(user) {
-                    sharp(req.file.buffer)
-                        .resize(280, 250)
-                        .max()
-                        .withoutEnlargement()
-                        .toBuffer()
-                        .then(function(thumbnail) {
-                            
-                        });
-                    });
-                }).catch(function(error) {
-                    res.render('userprofile', {
-                        user: req.body,
-                        errors: error.errors
-                    });
-                });
-            });
